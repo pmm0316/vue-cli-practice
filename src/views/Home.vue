eea33968d9abea186367af77860fe4f5d645b2b5
mingmingbuzai <847679250@qq.com>
2019-06-20 22:45:00 +0800

    引入全局scss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-row :gutter="0" class="container">
+    <el-col :span="3" class="left">
+      <div class="header">
+        header
+      </div>
+      <div class="menu">
+        菜单
+      </div>
+    </el-col>
+    <el-col :span="21" class="right">
+      <div class="header">
+        header
+      </div>
+      <main class="content">
+        content
+      </main>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home'
   }
-}
 </script>
+
+<style scoped lang="scss">
+  .container {
+    .el-col {
+      .header {
+        height: 60px;
+        background-color: $color-primary;
+      }
+    }
+    .left {
+      .header {
+        border-right: 1px solid #05509D;
+      }
+    }
+  }
+</style>
